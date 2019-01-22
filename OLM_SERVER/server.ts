e2c8855b5dacf80b7c52524c9fd0d8fa98394894
Nguyen Viet <vietnguyen11101992@gmail.com>
2019-01-22 23:46:45 +0700

    Revert "Tracking user widget"
    This reverts commit 3c6ab3982bc18ad576f4116913c167eaa1b0259a.

--- OLM_SERVER/server.ts
+++
@@ -1,7 +0,0 @@
-const express = require('express');
-import App from './app';
-
-// const port = process.env.PORT || 3000;
-const port = 8000;
-
-App.listen(port, () => {});
--- OLM_SERVER/startup.sh
+++
@@ -1,6 +0,0 @@
-#!/usr/bin/env bash
-if [ ! -d /usr/src/olm-server/node_modules ]; then
-  echo "Install dependencies..."
-  cd /usr/src/olm-server && npm install --no-bin-links
-fi
-cd /usr/src/olm-server && nodemon -L server.js
